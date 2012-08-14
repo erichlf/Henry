c6d3ee46280c1d5f34ee80ffd32507c6ca66b7bb
Erich L Foster <elfost@sandia.gov>
2012-08-14 15:12:27 -0600

    made major changes to Henry.f90. Nothing is working right now.

--- src/Henry.f90
+++ src/Henry.f90
@@ -39,23 +39,19 @@ PROGRAM Henrys_Problem
   USE LU
   IMPLICIT NONE
 
-  REAL, DIMENSION(:, :), ALLOCATABLE :: MATRIX_A, MATRIX_B !Matrices of Fourier Coefficients
-  REAL, DIMENSION(:, :), ALLOCATABLE :: B0h, Bl1h, Bl2h !Matrices
+  REAL, DIMENSION(:, :), ALLOCATABLE :: MATRIX_A, MATRIX_B, Matrix !Matrices of Fourier Coefficients
   REAL, DIMENSION(:, :), ALLOCATABLE :: quad, linear !Quadratic and linear terms
-  REAL, DIMENSION(:, :), ALLOCATABLE :: Psi, C, Psi_old, C_old !Stramlines and Isochlors
-  REAL, DIMENSION(:), ALLOCATABLE :: B0h_W, Bl1h_W, Bl2h_W !Constant vectors containing lin, quad and W_FUNC terms
-  INTEGER, DIMENSION(:), ALLOCATABLE :: indx0, indx1, indx2
+  REAL, DIMENSION(:, :), ALLOCATABLE :: Psi, C !Stramlines and Isochlors
+  INTEGER, DIMENSION(:), ALLOCATABLE :: indx
   REAL, DIMENSION(:), ALLOCATABLE :: x, y !Grid
   REAL, PARAMETER :: a = 0.263, b = 0.1, d = 1.0, l = 2.0, dx = 0.05, dy= 0.05 !Problem parameters
   REAL :: xi, pid4, fourdpi, xi2, api2, bpi2 !Constants
   REAL :: Psi_err, C_err, ERROR
   INTEGER :: h_a(5), h_b(0:4) !Size of h depends on both i and which coefficient A or B
-  INTEGER :: i_a, i_b, j_a, j_b, i_y, j_x, Bl1h_size, Bl2h_size !Size of the Matrices
-  INTEGER :: g, h, m, n, p, q, r, s, count, start !Loop counters
+  INTEGER :: i_a, i_b, j_a, j_b, i_y, j_x !Size of the Matrices
+  INTEGER :: g, h, m, n, p, q, r, s !Loop counters
   INTEGER :: AllocateStatus !Status variable for ALLOCATE
   REAL :: EPSILON = 5E-4
-  INTEGER :: loop = 4
-  REAL :: d0, d1, d2
 
   !WRITE (*,*), "Enter in the size of aquifer (d = thickness l = length) in meters."
   !READ *, d, l
@@ -85,19 +81,14 @@ PROGRAM Henrys_Problem
   api2 = a*pi**2
   bpi2 = b*pi**2
 
-  Bl1h_size = h_b(1) + h_b(2)
-  Bl2h_size = h_b(3) + h_b(4)
+
 
   ALLOCATE(MATRIX_A(i_a, 0:j_a), MATRIX_B(0:i_b, j_b), quad(0:i_b, j_b), linear(i_b, j_b), Psi(0:i_y, 0:j_x), &
-    Psi_old(0:i_y, 0:j_x), C(0:i_y, 0:j_x), C_old(0:i_y, 0:j_x), x(0:j_x), y(0:i_y), STAT = AllocateStatus)
-  ALLOCATE(B0h(h_b(0), h_b(0)), B0h_W(h_b(0)), Bl1h(Bl1h_size, Bl1h_size), Bl1h_W(Bl1h_size), Bl2h(Bl2h_size, Bl2h_size), &
-    Bl2h_W(Bl2h_size), indx0(h_b(0)), indx1(Bl1h_size), indx2(Bl2h_size), STAT = AllocateStatus)
+    C(0:i_y, 0:j_x), x(0:j_x), y(0:i_y), Matrix(i_a*(j_a+1)+(i_b+1)*j_b,i_a*(j_a+1)+(i_b+1)*j_b), STAT = AllocateStatus)
   IF(AllocateStatus /= 0) STOP "*** NOT ENOUGH MEMORY ***"
 
   MATRIX_A = 0. !Initializes array so that A = B = 0
   MATRIX_B = 0. 
-  Psi_old = 1.
-  C_old = 1.
 
   x_loop0: DO h = 0, j_x     !Initializing the values for x
     x(h) = dx*h
@@ -107,217 +98,10 @@ PROGRAM Henrys_Problem
     y(g) = dy*g
   END DO y_loop0
   
-  DO count = 0, 0!5!6
-    error = 1. !Initialize error terms
-    bpi2 = (b - count*0.01)*pi**2
-    WRITE (*,*) 'b=', b-count*0.01
-    DO g = 0, 4 !Initialize matrices and take their inverses
-      IF (g == 0 .OR. (g/2)*2 /= g) THEN 
-        DO m = 1, h_b(g)
-          h = m
-          DO n = 1, h_b(g) !Non-diagonal terms
-            IF (m /= n) THEN
-              s = n
-              IF (g == 0) THEN
-                B0h(m, n) = -eps(g)*s*N_FUNC(h, s)
-              ELSE IF (g == 1) THEN
-                Bl1h(m, n) = -eps(g)*s*N_FUNC(h, s)
-              ELSE
-                Bl2h(m, n) = -eps(g)*s*N_FUNC(h, s)
-              END IF
-            END IF
-          END DO
+  error = 1. !Initialize error terms
+  WRITE (*,*) 'b=', b
 
-          IF (g == 0) THEN !Diagonal terms
-            B0h(m, m) = eps(g)*bpi2*(g**2 + h**2/xi2)*xi
-          ELSE IF (g == 1) THEN
-            Bl1h(m, m) = eps(g)*bpi2*(g**2 + h**2/xi2)*xi
-          ELSE
-            Bl2h(m, m) = eps(g)*bpi2*(g**2 + h**2/xi2)*xi
-          END IF
-        END DO
-      ELSE
-        DO m = h_b(g - 1) + 1, h_b(g - 1) + h_b(g)
-          h = m - h_b(g - 1)
-          DO n = h_b(g - 1) + 1, h_b(g - 1) + h_b(g)
-            IF (m /= n) THEN !Diagonal terms
-              s = n - h_b(g - 1)
-              IF (g == 2) THEN
-                Bl1h(m, n) = -eps(g)*s*N_FUNC(h, s)
-              ELSE
-                Bl2h(m, n) = -eps(g)*s*N_FUNC(h, s)
-              END IF
-            END IF
-          END DO
-          IF (g == 2) THEN !Diagonal terms
-            Bl1h(m, m) = eps(g)*bpi2*(g**2 + h**2/xi2)*xi
-          ELSE
-            Bl2h(m, m) = eps(g)*bpi2*(g**2 + h**2/xi2)*xi
-          END IF
-        END DO
-      END IF
-    END DO
-
-    CALL ludcmp(B0h, h_b(0), h_b(0), indx0, d0)
-    CALL ludcmp(Bl1h, Bl1h_size, Bl1h_size, indx1, d1)
-    CALL ludcmp(Bl2h, Bl2h_size, Bl2h_size, indx2, d2)
-    !WRITE (*,*) 'b=', b
-        
-    Error_Loop: DO
-      WRITE (*,*) "Error=", error
-      IF (error <= Epsilon) THEN !Terminate loop when ABS(Error) <= Epsilon
-        EXIT Error_Loop
-      ELSE
-        DO q = 1, loop
-          DO g = 0, i_b !Update linear and quadratic terms in B(g, h)
-             DO h = 1, h_b(g)
-               quad(g, h) = pid4*SUM_AB(MATRIX_A, MATRIX_B, i_a, j_a, i_b, j_b, g, h) !Quadratic terms
-             END DO
-          END DO
-
-          g = 0
-          DO m = 1, h_b(g) !Setup the linear system B0h
-            h = m
-            !Constant terms for B0h including Quadratic and Linear terms
-            B0h_W(m) = fourdpi*W_FUNC(h, g) + quad(g, h)
-          END DO
-          CALL lubksb(B0h, h_b(g), h_b(g), indx0, B0h_W)
-
-          DO h = 1, h_b(g)
-            MATRIX_B(g, h) = B0h_W(h) !Place Boh_solution into corresponding B(g, h)
-          END DO
-
-          DO g = 1, i_a !Update A(g, h) with new B(0, h)
-            IF ((g*2)/2 /= g) THEN
-              start = 1
-            ELSE 
-              start = 0
-            END IF
-            DO h = start , h_a(g)
-              MATRIX_A(g, h) = 1./(eps(h)*api2*(g**2 + h**2/xi2)*xi)* &
-                (SUM_BN(MATRIX_B, i_b, j_b, g, h) + fourdpi*W_FUNC(g, h))
-            END DO
-          END DO
-        END DO
-
-        DO q = 1, loop
-          DO g = 0, i_b !Update linear and quadratic terms in B(g, h)
-            DO h = 1, h_b(g)
-              IF (g > 0) THEN
-                Linear(g, h) = SUM_AN(MATRIX_A, i_a, j_a, g, h)
-              END IF
-              quad(g, h) = pid4*SUM_AB(MATRIX_A, MATRIX_B, i_a, j_a, i_b, j_b, g, h) !Quadratic terms
-            END DO
-          END DO
-
-          DO m = 1, Bl1h_size
-            IF (m <= h_b(1)) THEN
-              g = 1
-              h = m
-            ELSE
-              g = 2
-              h = m - h_b(1)
-            END IF
-            !Constant terms for Bl1h including Quadratic and Linear terms
-            Bl1h_W(m) = fourdpi*W_FUNC(h, g) + quad(g, h) + linear(g, h)
-          END DO
-
-          CALL lubksb(Bl1h, Bl1h_size, Bl1h_size, indx1, Bl1h_W)
-
-          DO m = 1, Bl1h_size
-            IF (m <= h_b(1)) THEN
-              g = 1
-              h = m
-            ELSE
-              g = 2
-              h = m - h_b(1)
-            END IF
-            MATRIX_B(g, h) = Bl1h_W(m) !Place Boh_solution into corresponding B(g, h)
-          END DO
-
-          DO g = 1, i_a !Update A(g, h) with new B(0, h)
-            IF ((g*2)/2 /= g) THEN
-              start = 1
-            ELSE 
-              start = 0
-            END IF
-            DO h = 1 , h_a(g)
-              MATRIX_A(g, h) = 1./(eps(h)*api2*(g**2 + h**2/xi2)*xi)* &
-                (SUM_BN(MATRIX_B, i_b, j_b, g, h) + fourdpi*W_FUNC(g, h))
-            END DO
-          END DO
-        END DO
-
-        DO q = 1, loop
-          DO g = 0, i_b !Update linear and quadratic terms in B(g, h)
-            DO h = 1, h_b(g)
-              IF (g > 0) THEN
-                Linear(g, h) = SUM_AN(MATRIX_A, i_a, j_a, g, h)
-              END IF
-              quad(g, h) = pid4*SUM_AB(MATRIX_A, MATRIX_B, i_a, j_a, i_b, j_b, g, h) !Quadratic terms
-            END DO
-          END DO
-
-          DO m = 1, Bl2h_size
-            IF (m <= h_b(3)) THEN
-              g = 3
-              h = m
-            ELSE
-              g = 4
-              h = m - h_b(3)
-            END IF
-            !Constant terms for Bl2h including Quadratic and Linear terms
-            Bl2h_W(m) = fourdpi*W_FUNC(h, g) + quad(g, h) + linear(g, h)
-          END DO
-
-          CALL lubksb(Bl2h, Bl2h_size, Bl2h_size, indx2, Bl2h_W)
-
-          DO m = 1, Bl2h_size
-            IF (m <= h_b(3)) THEN
-              g = 3
-              h = m
-            ELSE
-              g = 4
-              h = m - h_b(3)
-            END IF
-            MATRIX_B(g, h) = Bl2h_W(m) !Place Boh_solution into corresponding B(g, h)
-          END DO
-
-          DO g = 1, i_a !Update A(g, h) with new B(0, h)
-            IF ((g*2)/2 /= g) THEN
-              start = 1
-            ELSE 
-              start = 0
-            END IF
-            DO h = start , h_a(g)
-              MATRIX_A(g, h) = 1./(eps(h)*api2*(g**2 + h**2/xi2)*xi)* &
-                      (SUM_BN(MATRIX_B, i_b, j_b, g, h) + fourdpi*W_FUNC(g, h))
-            END DO
-          END DO
-        END DO
-                        
-        x_loop: DO g = 0, i_y !Find Psi(g, h) and C(g, h)
-          y_loop: DO h = 0, j_x
-            IF (g == 0 .AND. h == 0) THEN
-              Psi(g, h) = 0.
-              C(g, h) = 0.
-            ELSE
-              Psi(g, h) = SUM_A(MATRIX_A, x, y, xi, g, h, i_a, j_a, i_y, j_x) + y(g)/d
-              C(g, h) = SUM_B(MATRIX_B, x, y, xi, g, h, i_b, j_b, i_y, j_x) + x(h)/(d*xi)
-            END IF
-          END DO y_loop
-        END DO x_loop
-
-        !Find the error
-        C_err = MAXVAL(ABS(C - C_old))
-        Psi_err = MAXVAL(ABS(Psi - Psi_old))
-
-        error = MAX(Psi_err, C_err)
-        C_old = C
-        Psi_old = Psi
-      END IF
-    END DO Error_Loop
-  END DO
+  CALL build_system() 
 
   CALL WRITETOFILE(Psi, C, x, y, i_y, j_x)
 
@@ -325,12 +109,30 @@ PROGRAM Henrys_Problem
   CLOSE (31)
   !CLOSE (32)
 
-  DEALLOCATE(MATRIX_A, MATRIX_B, quad, linear, Psi, C, Psi_old, C_old, x, y)
-  DEALLOCATE(B0h, B0h_W, indx0, Bl1h, Bl1h_W, indx1, Bl2h, Bl2h_W, indx2)
+  DEALLOCATE(MATRIX_A, MATRIX_B, quad, linear, Psi, C, x, y)
 
 !**************************************** Begin supporting FUNCTIONS ************************
         
   CONTAINS
+
+  SUBROUTINE build_system()
+    INTEGER :: i, j
+    
+    DO i=1, SIZE(Matrix,1)
+      DO j=1, SIZE(Matrix,2)
+        IF(i .LE. i_a*(j_b+1) .AND. j .LE. i_a*(j_b+1)) THEN
+          Matrix(i,j) = eps(j-1)*api2*(i**2 + (j-1)**2/xi2)*xi
+        ELSE IF(i .LE. i_a*(j_b+1)) THEN
+
+        ELSE IF(j .LE. i_a*(j_b+1) THEN
+
+        ELSE
+
+        END IF
+      END DO
+    END DO
+
+  END SUBROUTINE
 
   SUBROUTINE WRITETOFILE(Psi, C, x, y, i_y, j_x)
 
@@ -354,7 +156,6 @@ PROGRAM Henrys_Problem
       WRITE (30,*)
       WRITE (31,*)
     END DO
-
 
   END SUBROUTINE
 
